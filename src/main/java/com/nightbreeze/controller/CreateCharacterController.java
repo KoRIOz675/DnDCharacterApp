c2f7516e95d2943bbf8fae3ac635abb8bd58da3e
KoRIOz675 <thomas@mafille.net>
2025-05-01 16:15:55 +0200

    MINOR: Update .gitignore

--- src/main/java/com/nightbreeze/controller/CreateCharacterController.java
+++ src/main/java/com/nightbreeze/controller/CreateCharacterController.java
@@ -1,17 +1,19 @@
 package com.nightbreeze.controller;
 
 import com.nightbreeze.model.Character;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CreateCharacterController implements Initializable {
 
-    private Character character;
+    @FXML private TextField characterName;
+    @FXML private TextField characterClass;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        character = new Character();
     }
 }
